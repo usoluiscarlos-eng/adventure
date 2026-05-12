56bb235532f2f0a487d328cd2a0c0e85ecf2e7b0
usoluiscarlos-eng <usoluiscarlos@gmail.com>
2026-05-12 11:42:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
 const CACHE_NAME = 'adventure-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
